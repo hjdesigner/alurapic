46e6c8555682aaf182e60c1999f7536b3b6a33f9
Henrique Melanda <hjdesigner23@gmail.com>
2020-03-26 21:15:27 -0300

    Creating components and encapsulating the view

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos" :key="foto.id"><img :src="foto.url" :alt="foto.titulo" /></li>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li v-for="foto of fotos" :key="foto.id" class="lista-fotos-item">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './shared/painel/painel.vue'
 export default {
   name: 'HelloWorld',
+  components: {
+    'meu-painel': Painel
+  },
   data: () => {
     return {
       titulo: 'Alura Pic',
@@ -26,5 +34,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .centralizado {
+    text-align: center;
+  }
 
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
 </style>
---
+++ src/components/shared/painel/painel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo">{{ titulo }}</h2>
+    <slot class="painel-corpo">
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Painel',
+  props: {
+    titulo: String
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
+  }
+
+</style>
