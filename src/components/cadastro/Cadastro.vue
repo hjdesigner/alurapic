e6973c4159b2a39342fe8d110400e60de965d6d5
Henrique Melanda <hjdesigner23@gmail.com>
2020-04-05 12:30:26 -0300

    Validating form

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -4,29 +4,34 @@
     <h1 v-if="foto._id" class="centralizado">Alteração</h1>
     <h1 v-else class="centralizado">Inclusão</h1>
 
-    <form @submit.prevent="grava()">
-      <div class="controle">
-        <label for="titulo">TÍTULO</label>
-        <input v-model="foto.titulo" id="titulo" autocomplete="off">
-      </div>
+    <ValidationObserver v-slot="{ invalid, reset }">
+      <form @submit.prevent="grava()" @reset.prevent="reset">
+        <div class="controle">
+          <label for="titulo">TÍTULO</label>
+          <validation-provider rules="minmax:1,30" v-slot="{ errors }">
+            <input v-model="foto.titulo" id="titulo" autocomplete="off">
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+        </div>
 
-      <div class="controle">
-        <label for="url">URL</label>
-        <input v-model.lazy="foto.url" id="url" autocomplete="off">
-        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
-      </div>
+        <div class="controle">
+          <label for="url">URL</label>
+          <input v-model.lazy="foto.url" id="url" autocomplete="off">
+          <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
+        </div>
 
-      <div class="controle">
-        <label for="descricao">DESCRIÇÃO</label>
-        <textarea v-model="foto.descricao" id="descricao" autocomplete="off"></textarea>
-      </div>
+        <div class="controle">
+          <label for="descricao">DESCRIÇÃO</label>
+          <textarea v-model="foto.descricao" id="descricao" autocomplete="off"></textarea>
+        </div>
 
-      <div class="centralizado">
-        <meu-botao rotulo="GRAVAR" tipo="submit"/>
-        <router-link :to="{name: 'home'}"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
-      </div>
+        <div class="centralizado">
+          <meu-botao rotulo="GRAVAR" tipo="submit" :disabled="invalid" />
+          <router-link :to="{name: 'home'}"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+        </div>
 
-    </form>
+      </form>
+    </ValidationObserver>
   </div>
 </template>
 
@@ -36,11 +41,27 @@ import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
 import Botao from '../shared/botao/Botao.vue';
 import Foto from '../../domain/foto/Foto.js';
 import FotoService from '../../domain/foto/FotoService';
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+
+extend('required', {
+  ...required,
+  message: 'This field is required'
+});
+extend('minmax', {
+  ...required,
+  validate(value, { min, max }) {
+    return value.length >= min && value.length <= max;
+  },
+  params: ['min', 'max'],
+  message: 'Esse campo não pode ser vazio ou maior que 30 caracteres'
+});
 
 export default {
   name: 'Cadastro',
   components: {
-
+    ValidationProvider,
+    ValidationObserver,
     'imagem-responsiva': ImagemResponsiva, 
     'meu-botao': Botao
   },
